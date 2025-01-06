3338096fa5e195e4157498af2e99acdce5473654
elEsquina <o.azoubi@hotmail.com>
2025-01-06 17:21:36 +0100

    ye

--- project1/guessing-game.go
+++ project1/guessing-game.go
@@ -1,33 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "math/rand/v2"
+	"fmt"
+	"math/rand"
 	"os"
+	"strconv"
 )
 
-func main(){
-
+func main() {
 	var inp string
 	var curr, best, limit, rang int
 
-	rang, limit = selectDifficulty()
-	best = game(rang, limit)
+
+	best = loadBestScore()
+	if best == -1 {
+		rang, limit = selectDifficulty()
+		best = game(rang, limit)
+	}
 
 	for {
-
-		fmt.Println("Play again (y / yes) ? Your best score is", best)
+		fmt.Println("Play again (y / yes)? Your best score is", best)
 		fmt.Scan(&inp)
 
 		if inp == "y" || inp == "yes" {
 			rang, limit = selectDifficulty()
 			curr = game(rang, limit)
-			best = min(curr, best)	
-		}
-		else {
+			best = min(curr, best)
+
+			saveBestScore(best)
+		} else {
 			break
 		}
+	}
+}
 
+func loadBestScore() int {
+	file, err := os.Open("bestScore.txt")
+	if err != nil {
+		return -1
+	}
+	defer file.Close()
+
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil || count == 0 {
+		return -1
+	}
+
+	score, err := strconv.Atoi(string(data[:count]))
+	if err != nil {
+		return -1
+	}
+
+	return score
+}
+
+func saveBestScore(score int) {
+	file, err := os.Create("bestScore.txt")
+	if err != nil {
+		fmt.Println("Error saving score:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write([]byte(strconv.Itoa(score)))
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
 	}
 }
 
@@ -38,34 +76,32 @@ func selectDifficulty() (int, int) {
 	fmt.Scan(&diff)
 
 	switch diff {
-		case "easy":
-			return 10, 5
-		case "medium":
-			return 50, 10
-		case "hard":
-			return 100, 15
-		default:
-			fmt.Println("Invalid difficulty")
-			os.Exit(1)
-			return 0, 0
+	case "easy":
+		return 10, 5
+	case "medium":
+		return 50, 10
+	case "hard":
+		return 100, 15
+	default:
+		fmt.Println("Invalid difficulty")
+		os.Exit(1)
+		return 0, 0
 	}
 }
 
-
-func game(rang int, limit int) int{
-
+func game(rang int, limit int) int {
 	fmt.Println("Guess a number between 0 and", rang)
 
-	var random int = rand.IntN(rang)
+	random := rand.Intn(rang)
 	var guess, count int
 
-	for { 
-
-		if count > limit {
-			fmt.Println("Looser")	
+	for {
+		if count >= limit {
+			fmt.Println("You lose! The correct number was", random)
 			break
 		}
 
+		fmt.Print("Enter your guess: ")
 		fmt.Scan(&guess)
 		count++
 
@@ -77,7 +113,6 @@ func game(rang int, limit int) int{
 			fmt.Println("You got it!")
 			break
 		}
-
 	}
 
 	return count
